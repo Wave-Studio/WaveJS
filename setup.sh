a67ff9551c11f8aadcb7d3d8d34f46f7ba3b75ac
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2022-03-06 19:19:15 -0600

    im small brain

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Not tested on windows!
 echo "Cloning WaveJS vanilla example ..."
-git clone https://wave-studio/wavejs
+git clone https://github.com/wave-studio/wavejs
 cp -a -r ./wavejs/examples/vanilla/. ./
 rm -rf ./wavejs
 echo "Cloned! Run npm install or yarn to install packages."
